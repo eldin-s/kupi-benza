3b4de29ebda4c3fd8c0fe9e2198e673e803d647e
eldin-s <eldin_s12@hotmail.com>
2025-04-22 16:00:09 +0200

    create send-reservation-email

---
+++ supabase/functions/send-reservation-email/index.js
@@ -0,0 +1,47 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+// Setup type definitions for built-in Supabase Runtime APIs
+/// <reference types="https://esm.sh/@supabase/functions-js/src/edge-runtime.d.ts" />
+
+const RESEND_API_KEY = Deno.env.get('RESEND_API_KEY')
+
+const handler = async (_request: Request): Promise<Response> => {
+  const res = await fetch('https://api.resend.com/emails', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${RESEND_API_KEY}`,
+    },
+    body: JSON.stringify({
+      from: 'onboarding@resend.dev',
+      to: 'delivered@resend.dev',
+      subject: 'hello world',
+      html: '<strong>it works!</strong>',
+    }),
+  })
+
+  const data = await res.json()
+
+  return new Response(JSON.stringify(data), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+}
+
+Deno.serve(handler)
+
+/* To invoke locally:
+
+  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
+  2. Make an HTTP request:
+
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/send-reservation-email' \
+    --header 'Authorization: Bearer ' \
+    --header 'Content-Type: application/json' \
+    --data '{"name":"Functions"}'
+
+*/
